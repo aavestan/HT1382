33e0f02ebd9b1c3ab0610ba5cbc4c21023c0a4f4
aavestan <ali.avestan@gmail.com>
2020-08-27 15:29:41 +0430

    Update Set_Clock.ino

--- examples/Set_Clock/Set_Clock.ino
+++ examples/Set_Clock/Set_Clock.ino
@@ -1,9 +1,18 @@
+#include "HT1382.h"
+HTClock rtc;
+
 void setup() {
   // put your setup code here, to run once:
-
+   Serial.begin(115200);  // start serial for output
+   Serial.println("Start ..");
+   Serial.println("Seting Clock");
+//set(const uint8_t second, const uint8_t minute, const uint8_t hour, const uint8_t dayOfWeek, const uint8_t dayOfMonth, const uint8_t month, const uint8_t year)    
+rtc.set(0,22,15,5,27,8,20);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   Serial.println("Time :"+String(rtc.hour())+":"+String(rtc.minute())+":"+String(rtc.second()));
+   Serial.println("Date :"+String(rtc.year())+"/"+String(rtc.month())+"/"+String(rtc.day()));
+   delay(1000);
 
 }
